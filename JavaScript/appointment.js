f23980729dc5947bb39ea538a752f2ee43a487fb
Miracle Igboanusi <miracleigboanusi@gmail.com>
2025-08-22 06:19:27 +0100

    little changes

--- JavaScript/appointment.js
+++ JavaScript/appointment.js
@@ -1,9 +1,10 @@
-document.addEventListener("DOMContentLoaded", function () {
-    const username = sessionStorage.getItem("loggedInUser");
+const username = localStorage.getItem("username");
+const useremail = localStorage.getItem("useremail");
 
-    if (username) {
-        document.getElementById("dashboard-username").textContent = username;
-    } else {
-        document.getElementById("dashboard-username").textContent = "Patient Name";
-    }
-});
+if (username && useremail) {
+    document.getElementById("userName").textContent = username;
+    document.getElementById("userEmail").textContent = useremail;
+} else {
+    document.getElementById("userName").textContent = "No name found.";
+    document.getElementById("userEmail").textContent = "No email found.";
+}
